c2b1af1c134088b9b0795377f83c2052f3f87921
Andy Gayton <andy@thecablelounge.com>
2021-10-23 20:41:26 -0400

    begin to add a http subcommand

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,17 @@ fn main() {
     let matches = App::new("x")
         .version("0.0.1")
         .about("swiss army knife for the command line")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::DisableHelpSubcommand)
+        .subcommand(App::new("http").about(
+            "Serve HTTP. Requests are written to STDOUT and \
+            responses are read from STDIN",
+        ))
         .subcommand(
             App::new("tcp")
-                .about("swiss army knife for the command line")
+                .about("TCP utilities")
                 .setting(AppSettings::SubcommandRequiredElseHelp)
+                .setting(AppSettings::DisableHelpSubcommand)
                 .arg(
                     Arg::new("port")
                         .short('p')
@@ -21,11 +28,11 @@ fn main() {
                         .required(true)
                         .takes_value(true),
                 )
-                .subcommand(App::new("spread").about(
-                    "Read lines from STDIN and writes them to all TCP connections",
-                ))
                 .subcommand(App::new("merge").about(
                     "Read lines from TCP connections and writes them serially to STDOUT",
+                ))
+                .subcommand(App::new("spread").about(
+                    "Read lines from STDIN and writes them to all TCP connections",
                 )),
         )
         .get_matches();
@@ -34,12 +41,39 @@ fn main() {
         Some(("tcp", matches)) => {
             let port: u32 = matches.value_of_t("port").unwrap_or_else(|e| e.exit());
             match matches.subcommand_name() {
-                Some("spread") => do_spread(port),
                 Some("merge") => do_merge(port),
+                Some("spread") => do_spread(port),
                 _ => unreachable!(),
             }
         }
         _ => unreachable!(),
+    }
+}
+
+fn do_merge(port: u32) {
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let buf = BufReader::new(&stream);
+                for line in buf.lines() {
+                    let line = line.unwrap();
+                    tx.send(line).unwrap();
+                }
+            });
+        }
+    });
+
+    let stdout = io::stdout();
+    for line in rx {
+        if writeln!(&stdout, "{}", line).is_err() {
+            break;
+        }
     }
 }
 
@@ -75,32 +109,5 @@ fn do_spread(port: u32) {
             .lock()
             .expect("poisoned")
             .retain(|conn| conn.send(line.clone()).is_ok());
-    }
-}
-
-fn do_merge(port: u32) {
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
-
-    let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            let tx = tx.clone();
-            thread::spawn(move || {
-                let buf = BufReader::new(&stream);
-                for line in buf.lines() {
-                    let line = line.unwrap();
-                    tx.send(line).unwrap();
-                }
-            });
-        }
-    });
-
-    let stdout = io::stdout();
-    for line in rx {
-        if writeln!(&stdout, "{}", line).is_err() {
-            break;
-        }
     }
 }
